68f547ff4dd398293accce832c814ae5814c19f2
William Burton <william17.burton@gmail.com>
2018-07-29 23:42:57 +0100

    Fix foodcritic/rubocop

--- resources/oc_serviceaccount.rb
+++ resources/oc_serviceaccount.rb
@@ -5,12 +5,12 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 provides :oc_serviceaccount
-property :name, String, required: true
+property :service_accountname, String, required: true, name_property: true
 property :namespace, String, required: true
 
 action :create do
-  execute "Create ServiceAccount [#{new_resource.name}]" do
-    command "#{node['cookbook-openshift3']['openshift_common_client_binary']} create serviceaccount #{new_resource.name} -n #{new_resource.namespace} --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig"
-    not_if "#{node['cookbook-openshift3']['openshift_common_client_binary']} get serviceaccount/#{new_resource.name} --no-headers --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig -n #{new_resource.namespace}"
+  execute "Create ServiceAccount [#{new_resource.service_accountname}]" do
+    command "#{node['cookbook-openshift3']['openshift_common_client_binary']} create serviceaccount #{new_resource.service_accountname} -n #{new_resource.namespace} --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig"
+    not_if "#{node['cookbook-openshift3']['openshift_common_client_binary']} get serviceaccount/#{new_resource.service_accountname} --no-headers --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig -n #{new_resource.namespace}"
   end
 end
