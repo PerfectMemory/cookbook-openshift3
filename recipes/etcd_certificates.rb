a062912cc0b920b767ffa7d0deca0bf282e4a64b
William Burton <william17.burton@gmail.com>
2018-10-21 19:35:13 +0100

    Fix rubocop

--- recipes/etcd_certificates.rb
+++ recipes/etcd_certificates.rb
@@ -123,27 +123,13 @@ if is_certificate_server
     end
   end
 
-  remote_file "#{node['cookbook-openshift3']['etcd_certificate_dir']}/ca.crt" do
-    owner 'root'
-    group 'root'
-    source "file://#{node['cookbook-openshift3']['etcd_ca_dir']}/ca.crt"
-    mode '0644'
-    sensitive true
-  end
-
-  remote_file "#{node['cookbook-openshift3']['etcd_certificate_dir']}/peer.key" do
-    owner 'root'
-    group 'root'
-    source "file://#{node['cookbook-openshift3']['etcd_generated_certs_dir']}/etcd-#{etcd_servers.first['fqdn']}/peer.key"
-    mode '0644'
-    sensitive true
-  end
-
-  remote_file "#{node['cookbook-openshift3']['etcd_certificate_dir']}/peer.crt" do
-    owner 'root'
-    group 'root'
-    source "file://#{node['cookbook-openshift3']['etcd_generated_certs_dir']}/etcd-#{etcd_servers.first['fqdn']}/peer.crt"
-    mode '0644'
-    sensitive true
+  %w[ca.crt peer.key peer.crt].eacho do |file|
+    remote_file "#{node['cookbook-openshift3']['etcd_certificate_dir']}/#{file}" do
+      owner 'root'
+      group 'root'
+      source "file://#{node['cookbook-openshift3']['etcd_ca_dir']}/#{file}"
+      mode '0644'
+      sensitive true
+    end
   end
 end
